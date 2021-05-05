f11af1d42988e6ba76949ef140c7547669ffef78
eynol <625608852@qq.com>
2021-05-06 01:51:30 +0800

    init

---
+++ src/components/ReadFile.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  import bookmark from "netscape-bookmark-tree/dist/bookmark.ast.esm";
+  let fileInput = "";
+  function readFile(e) {
+    const file = e.target.files[0];
+    console.log(e.target.files[0]);
+    const fileReader = new FileReader();
+    fileReader.readAsText(file);
+    fileReader.onload = (e) => {
+      console.log(bookmark(fileReader.result));
+      //   dispatch("read", {
+      //       result:fileReader.result
+      //   });
+      //   console.log(fileReader.result);
+    };
+  }
+</script>
+
+<input type="file" bind:value={fileInput} on:change={readFile} />
---
+++ src/index.svelte
@@ -0,0 +1,8 @@
+
+<script>
+    import ReadFile from './components/ReadFile.svelte';
+    function handleRead ({detail:{result}}){
+        console.log('xx',result)
+    }
+</script>
+<ReadFile on:read={handleRead} />
