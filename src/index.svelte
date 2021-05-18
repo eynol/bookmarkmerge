436b1af7c325394275d42361288024f4b881ccb7
eynol <625608852@qq.com>
2021-05-18 13:32:01 +0800

    feat:down load file

--- src/index.svelte
+++ src/index.svelte
@@ -11,15 +11,29 @@
 
   let treeHtml = "";
   function displayTree() {
-    console.time('generateTemplate')
+    console.time("generateTemplate");
     treeHtml = generateTemplate($workingTree);
-    console.timeEnd('generateTemplate')
+    console.timeEnd("generateTemplate");
+  }
+  function downloadFile() {
+    const a = document.createElement("a");
+    const file = new Blob([treeHtml]);
+    a.href = URL.createObjectURL(file);
+    const today = new Date();
+    const todayTxt = `${today.getFullYear()}-${
+      today.getMonth() + 1
+    }-${today.getDate()}`;
+    a.download = "bookmarks_merged_" + todayTxt+'.html';
+    document.body.append(a);
+    a.click()
   }
   //   import { debug } from "_svelte@3.38.2@svelte/internal";
   //   debug($files);
 </script>
 
-<ReadFile on:read={handleRead} /><xy-button on:click={displayTree}>Generate</xy-button>
+<ReadFile on:read={handleRead} />
+<xy-button on:click={displayTree}>Generate</xy-button>
+<xy-button on:click={downloadFile}>Download</xy-button>
 <br />
 <Folder open {...$workingTree || {}} />
 
